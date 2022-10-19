98c563c850cc762e03f1f95dc2dcdbaee0ba1b9b
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-10-19 16:11:46 -0400

    Deleted Scratch File

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,10 +1,8 @@
 package com.example.demo.security;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.demo.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.demo.security.ApplicationUserRole.*;
 
 @Configuration
@@ -47,33 +44,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails tanyaUser = User.builder()
                 .username("tanya")
                 .password(passwordEncoder.encode("password"))
-<<<<<<< HEAD
-                .roles(STUDENT.name()) // ROLE_STUDENT
-=======
-//                .roles(STUDENT.name()) // ROLE_STUDENT
->>>>>>> a368e71 (Restored Missing Branches)
+//              .roles(STUDENT.name()) // ROLE_STUDENT
                 .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails terenceUser = User.builder()
                 .username("terence")
                 .password(passwordEncoder.encode("password123"))
-<<<<<<< HEAD
-                .roles(ADMIN.name()) // ROLE_ADMIN
-=======
-//                .roles(ADMIN.name()) // ROLE_ADMIN
->>>>>>> a368e71 (Restored Missing Branches)
+//              .roles(ADMIN.name()) // ROLE_ADMIN
                 .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails patrickUser = User.builder()
                 .username("patrick")
                 .password(passwordEncoder.encode("password456"))
-<<<<<<< HEAD
-                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
-=======
-//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
->>>>>>> a368e71 (Restored Missing Branches)
+//              .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
                 .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
@@ -82,7 +67,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 terenceUser,
                 patrickUser
         );
-
     }
 
     private static HttpSecurity getHttp(HttpSecurity http) {
